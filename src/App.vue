0760f5e415aa23386f9bb842bc604c95be7a98f5
Jossar Mukana Muheta <muhetajossar@gmail.com>
2020-10-16 14:20:16 +0200

    mise en place navbar

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,27 @@
 <template>
   <v-app>
-    <v-main>
-      <HelloWorld/>
-    </v-main>
+    <div class="main_container">
+      <AppHeader/>
+      <section>
+      <router-view />
+      </section>
+    <AppFooter/>
+    </div>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
 
+
+import AppHeader from "@/components/Layout/AppHeader";
+import AppFooter from "@/components/Layout/AppFooter";
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    AppFooter,
+    AppHeader
+
   },
 
   data: () => ({
@@ -21,3 +29,17 @@ export default {
   }),
 };
 </script>
+<style lang="scss" scoped>
+
+.main_container{
+  display: grid !important;
+  grid-auto-rows: 0.2fr 3fr 0.3fr ;
+  grid-gap: 5px;
+  min-height: 100vh;
+}
+  body{
+    margin: 0;
+    padding: 0;
+    min-height: 100vh;
+  }
+</style>
--- src/components/Layout/AppHeader.vue
+++ src/components/Layout/AppHeader.vue
@@ -1,13 +1,93 @@
 <template>
+  <div>
+    <v-navigation-drawer
+        app
+        v-model="drawer"
+        class="white lighten-2"
+        dark
+        disable-resize-watcher
+    >
+      <v-toolbar-title class="name">Oise <span style="color:#FA2021">Télécom</span></v-toolbar-title>
 
+      <v-list nav dense>
+        <v-list-item-group color="secondary">
+          <v-list-item v-for="(items,item) in drawerItems" :key="item">
+            <v-list-item-content>
+              <v-list-item-title>
+                <router-link :to="items.link">
+                  {{ items.name}}
+                </router-link>
+              </v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+      <v-spacer></v-spacer>
+    </v-navigation-drawer>
+    <v-app-bar
+        prominent
+        height="80px"
+        class="align-center justify-space-around"
+
+
+
+    >
+      <v-app-bar-nav-icon
+          class="hidden-md-and-up"
+          @click.stop="drawer = !drawer"
+      >
+
+      </v-app-bar-nav-icon>
+
+      <v-toolbar-title class="align-self-center">Oise <span style="color:#FA2021">Télécom</span></v-toolbar-title>
+
+
+      <div>
+        <v-btn  depressed class="hidden-sm-and-down" text v-for="(navItem, elt) in drawerItems" :key="elt"
+        >
+          <router-link :to="navItem.link">
+            {{ navItem.name }}
+          </router-link>
+        </v-btn>
+
+      </div>
+
+    </v-app-bar>
+  </div>
 </template>
 
 <script>
+
 export default {
-name: "AppHeader"
+  name: "AppHeader",
+  components: {},
+  data : function (){
+    return{
+      drawer: false,
+      drawerItems : [
+        {name: "Accueil", link: "/"},
+        {name: "Nos boutiques", link: "/Boutiques"},
+        {name: "Services", link: "/Services"},
+        {name: "Reconditionner", link: "/Reconditionner"},
+        {name: "Contact", link: "/Contact"}
+      ]
+    }
+  }
 }
 </script>
 
-<style lang='scss' scoped>
+<style lang="scss" scoped>
+@import "src/styles/variables.scss";
+
+a{
+  text-decoration: none;
+  color: $black !important;
+  transition: all 300ms ease-in-out;
+
+  &:hover{
+    color: $red-custom !important;
+    transform: scale(1.1);
+  }
+}
 
 </style>
